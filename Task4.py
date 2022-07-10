c07b8dfe0283b6775fd14b30baf3417afa71fdb4
Nikolay_Davydov <skmoscow74@yandex.ru>
2022-07-10 16:45:53 +0300

    homework 2

--- Task4.py
+++ Task4.py
@@ -12,8 +12,8 @@ for i in range (n):
 print("Получившийся список ",list)
 while True:
     a = int (input ("Введите первую позицию "))
-    if a>=0 and a<=n: break
+    if a>0 and a<=n: break
 while True:
     b = int (input ("Введите вторую позицию "))
-    if b>=0 and b<=n: break
+    if b>0 and b<=n: break
 print ("Произведение элементов на указанных позициях ", multyply(a-1,b-1)) #если считать позицию больше индекса на 1
